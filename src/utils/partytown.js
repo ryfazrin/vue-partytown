5b5102de61d2dfb8e5be82cb4fa411280d3e92bc
ryfazrin <riansera515@gmail.com>
2022-04-09 00:40:25 +0800

    configuration debug false

--- src/utils/partytown.js
+++ src/utils/partytown.js
@@ -3,16 +3,7 @@ import { partytownSnippet } from '@builder.io/partytown/integration'
 const Partytown = () => {
   const scriptElm = document.createElement('script')
   scriptElm.dataset.partytown = ''
-  scriptElm.innerHTML = partytownSnippet({
-    debug: true,
-    forward: ['dataLayer.push'],
-    // logCalls: true,
-    // logGetters: true,
-    // logSetters: true,
-    // logStackTraces: false,
-    logScriptExecution: true,
-    // logMainAccess: true,
-  })
+  scriptElm.innerHTML = partytownSnippet()
   document.head.appendChild(scriptElm)
 }
 
