7070be807ce9057473e511af69a4533c709b2b60
ryfazrin <riansera515@gmail.com>
2022-04-11 10:38:12 +0800

    try partytown gtm

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </nav>
-
-    <strong>window.fetch(url) text</strong>
-    <code>
-      <span id="testFetchText"></span>
-    </code>
+    </nav> -->
 
     <use-partytown 
       script="
-        (async function () {
-          const elm = document.getElementById('testFetchText');
-          const rsp = await window.fetch('text.txt');
-          const text = await rsp.text();
-          elm.textContent = text;
-          elm.className = 'testFetchText';
+        (function (window, document, i) {
+          window['dataLayer'] = window[dataLayer] || [];
+
+          window['dataLayer'].push({ 'gtm.start': new Date().getTime(), 'event': 'gtm.js' });
+
+          var firstScript = document.getElementsByTagName('script')[0];
+          var gtmScript = document.createElement('script');
+          var dataLayer = 'dataLayer' != 'dataLayer' ? '&l=' + 'dataLayer' : '';
+
+          gtmScript.async = true;
+
+          // ORIGINAL
+          gtmScript.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dataLayer;
+
+          // HACK
+          // gtmScript.src = './gtm.js?id=' + i + dataLayer;
+
+          firstScript.parentNode.insertBefore(gtmScript, firstScript);
+        })(window, document, 'GTM-W275NLW');
+      "
+    />
+
+    <h1>Google Tag Manager (GTM) 🎉</h1>
+
+    <p>
+      <strong>dataLayer.push</strong>
+      <span id="testDataLayer"></span>
+    </p>
+
+    <button id="buttonDataLayerPush" @click="gtmPush">Partytown dataLayer.push()</button>
+
+    <use-partytown
+      script="
+        (function () {
+          document.body.classList.add('completed');
         })();
       "
     />
@@ -35,6 +59,16 @@ export default {
   // components: {
   //   MyPartytown
   // }
+  methods: {
+    gtmPush: function () {
+        const data = { event: 'button-click', from: 'partytown' };
+        console.log(`GTM dataLayer.push(${JSON.stringify(data)})`);
+        dataLayer.push({ event: 'button-click', from: 'partytown' });
+
+        const testDataLayer = document.getElementById('testDataLayer');
+        testDataLayer.textContent = 'pushed';
+      }
+  },
 }
 </script>
 
--- src/utils/partytown.js
+++ src/utils/partytown.js
@@ -6,6 +6,26 @@ const Partytown = () => {
   scriptElm.innerHTML = partytownSnippet({
     debug: true,
     forward: ['dataLayer.push'],
+    resolveUrl(url) {
+      if (
+        url.hostname.includes('google-analytics') ||
+        url.hostname.includes('www.googletagmanager.com')
+      ) {
+        const proxyUrl = new URL('https://cdn.builder.io/api/v1/proxy-api');
+        proxyUrl.searchParams.append('url', url);
+        return proxyUrl;
+      }
+      
+      return url;
+    },
+    logCalls: true,
+    logGetters: true,
+    logSetters: true,
+    logImageRequests: true,
+    logMainAccess: true,
+    logSendBeaconRequests: true,
+    logStackTraces: false,
+    logScriptExecution: true,
   })
   document.head.appendChild(scriptElm)
 }
